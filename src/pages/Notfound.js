a4aa6e871d33410ce39c3d754ab72afd8f963424
Simon Johnston <simonjohnston.c@gmail.com>
2023-01-21 19:48:45 -0800

    404 lottie

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import animationData from '../assets/T-rex.json'
+
 
 const Notfound = () => {
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+
   return (
-    <div>This is 404</div>
+    <FourContainer>
+      <Lottie 
+          options={defaultOptions}
+          style={{margin:'2.5%', height: '600px', width: '95%' }}
+          />
+    </FourContainer>
   )
 }
+
+const FourContainer = styled.div`
+  margin-top: 10px;
+  margin-right: 5%;
+  margin-left: 5%;
+  background:black;
+  border-radius: 20px 20px 20px 20px;
+`
 
 export default Notfound
